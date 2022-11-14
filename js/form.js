289d288b0eb1ee823cd99edf820426e7496dd572
nookismile <stina.novickova@yandex.ru>
2022-11-15 01:18:26 +0300

    add validate form

--- js/form.js
+++ js/form.js
@@ -3,16 +3,65 @@ const formInteractive = [...form.children];
 const mapFilters = document.querySelector('.map__filters');
 const mapFiltersInteractive = [...mapFilters.children];
 
-const addInactiveStay = () => {
-  form.classList.add('ad-form--disabled');
-  formInteractive.forEach((element) => {
-      element.disabled = true;
-  });
+const MIN_VALUE_TITLE = 30;
+const MAX_VALUE_TITLE = 100;
+const MAX_VALUE_PRICE = 100000;
 
-  mapFilters.classList.add('ad-form--disabled');
-  mapFiltersInteractive.forEach((element) => {
-      element.disabled = true;
-  });
+const pristine = new Pristine(form, {
+    classTo: 'ad-form__element',
+    errorClass: 'ad-form__element--invalid',
+    successClass: 'ad-form__element--valid',
+    errorTextParent: 'ad-form__element',
+    errorTextTag: 'span',
+    errorTextClass: 'ad-form__error'
+}, false);
+
+function validateTitle(value) {
+    return value.length >= MIN_VALUE_TITLE && value.length <= MAX_VALUE_TITLE;
+}
+
+pristine.addValidator(
+    form.querySelector('#title'),
+    validateTitle,
+    'От 30 до 100 символов'
+);
+
+function validatePrice(value) {
+    return value <= MAX_VALUE_PRICE;
+}
+
+pristine.addValidator(
+    form.querySelector('#price'),
+    validatePrice,
+    'до 100 000'
+);
+
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    pristine.validate();
+    console.log('ok');
+});
+
+
+
+
+
+
+
+
+const addInactiveStay = () => {
+    form.classList.add('ad-form--disabled');
+    formInteractive.forEach((element) => {
+        element.disabled = true;
+    });
+
+    mapFilters.classList.add('ad-form--disabled');
+    mapFiltersInteractive.forEach((element) => {
+        element.disabled = true;
+    });
 };
 
 const addActiveStay = () => {
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 import { generateCardElements } from './generate-card-elements.js';
 import { getData } from './data.js';
 import { createAdvertisement } from './create-advertisement.js';
-import { addInactiveStay } from './form.js';
+import { addActiveStay } from './form.js';
 
 const {SIMILAR_ADVERTISEMENTS} = getData();
 
-addInactiveStay();
+addActiveStay();
 
 const advertisements = Array.from({length: SIMILAR_ADVERTISEMENTS}, createAdvertisement);
 
